136b7c533a936f451165eda471c344408b36f61c
tranhoangquangdieu <62338322+tranhoangquangdieu@users.noreply.github.com>
2020-04-08 16:56:42 +0700

    Add files via upload

---
+++ SS6ReadProp.js
@@ -0,0 +1,13 @@
+function read1()
+{
+    let inp1=document.getElementById("bai1");
+    console.log(inp1);
+    console.log(inp1.value);
+}
+
+function read2()
+{
+    let h1=document.getElementById("heading1");
+    console.log(h1);
+    console.log(h1.fontsize);
+}
